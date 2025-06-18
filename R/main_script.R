e8e3b07ff6fb8527e17d760acbd549bd7b754aa7
mcaudevillaUnizar <mcaudevilla@unizar.es>
2025-06-18 09:39:01 +0200

    Update main_script.R

--- R/main_script.R
+++ R/main_script.R
@@ -1,1 +1,15 @@
+# main_script.R
+
+install_and_load <- function(packages) {
+  for (pkg in packages) {
+    if (!requireNamespace(pkg, quietly = TRUE)) {
+      install.packages(pkg)
+    }
+    library(pkg, character.only = TRUE)
+  }
+}
+
+packages <- c("data.table", "purrr", "furrr", "sf", "parallel", "arrow")
+
+install_and_load(packages)
 
